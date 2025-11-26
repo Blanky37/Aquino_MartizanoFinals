aa325ab7d9bf216cf431dedb96cced75234f95c9
FioMartizano <144452744+FioMartizano@users.noreply.github.com>
2025-11-26 08:14:32 +0800

    User Class (Model)

---
+++ Aquino_MartizanoFinals/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Aquino_MartizanoFinals.Models
+{
+    public class User
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public AttendanceStatus Week1 { get; set; }
+        public AttendanceStatus Week2 { get; set; }
+        public AttendanceStatus Week3 { get; set; }
+
+    }
+    //attendance status
+    public enum AttendanceStatus
+    {
+        Present,
+        Excused,
+        Absent
+    }
+
+}
